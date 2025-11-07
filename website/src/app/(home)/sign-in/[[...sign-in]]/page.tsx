00890b4ea4c536d39ea9445b143457e042ce076a
jishkhar <jishnukhargharia11@gmail.com>
2025-11-07 23:08:33 +0530

    feat: sign-in-up-page

---
+++ website/src/app/(home)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { dark } from "@clerk/themes";
+import { SignIn } from "@clerk/nextjs";
+import { useCurrentTheme } from "@/lib/hooks/use-current-theme";
+
+const Page = () => {
+    const currentTheme = useCurrentTheme();
+
+    return (
+        <div className="flex flex-col max-w-3xl mx-auto w-full">
+            <section className="space-y-6 pt-[16vh] 2xl:pt-32">
+                <div className="flex flex-col items-center">
+                    <SignIn
+                        appearance={{
+                            baseTheme: currentTheme === "dark" ? dark : undefined,
+                            elements: {
+                                cardBox: "border! ! rounded-lg!",
+                            },
+                        }}
+                    />
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Page;
