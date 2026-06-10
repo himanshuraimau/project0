f79a51900309afee127563760b921780b25c877a
Himanshu Rai <himanshuraimau9@gmail.com>
2026-06-10 19:09:41 +0530

    feat(app.config): add react-native-purchases to app configuration

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -63,6 +63,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         "expo-secure-store",
         "expo-web-browser",
         "expo-localization",
+        "react-native-purchases",
         [
             "expo-av",
             {
